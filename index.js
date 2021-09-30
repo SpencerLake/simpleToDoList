077d4652be3324bfd4d32e61311289141b10329e
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-09-30 18:17:20 -0400

    copied to make both list priorities functional

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 console.log("hello world")
 
+
+//High Priority List =========================================================
+
 let addList = document.querySelector('#ahForm')
 addList.addEventListener('submit', highList)
 
@@ -9,17 +12,50 @@ function highList(input) {
     let stuff = document.querySelector('#h-input').value
     let newLi = document.createElement('li')
     const text = document.createTextNode(stuff)
-    
-    newLi.appendChild(text)
-
     const element = document.getElementById('h-p')
 
+    newLi.appendChild(text)
     element.appendChild(newLi)
-
     console.log(stuff)
+
+    
+    let delet = document.createElement('button')
+    delet.textContent = 'X'
+    delet.addEventListener('click', deleteList)
+    newLi.appendChild(delet)
 }
 
+//Low Priority List ===========================================================
 
+let addlList = document.querySelector('#alForm')
+addlList.addEventListener('submit', lowList)
+
+function lowList(input) {
+    input.preventDefault()
+    
+    let stuff = document.querySelector('#l-input').value
+    let newLi = document.createElement('li')
+    const text = document.createTextNode(stuff)
+    const element = document.getElementById('l-p')
+
+    newLi.appendChild(text)
+    element.appendChild(newLi)
+    console.log(stuff)
+
+    
+    let delet = document.createElement('button')
+    delet.textContent = 'X'
+    delet.addEventListener('click', deleteList)
+    newLi.appendChild(delet)
+}
+
+//Global Pieces =============================================================
+
+function deleteList(event) {
+    event.target.parentNode.remove()
+    // Message.textContent = 'Deleted'
+    // revealMessage()
+}
 
 
 
